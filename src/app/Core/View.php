d6fba02288151a40e2f68af0cc92e32c5347574a
Strannik <strannik.mas@gmail.com>
2026-04-17 22:03:13 +0700

    fix setCompileDir

--- src/app/Core/View.php
+++ src/app/Core/View.php
@@ -11,9 +11,17 @@ class View
     
     public function __construct()
     {
+        $compileDir = __DIR__ . '/../../templates_c';
+        if (!is_dir($compileDir)) {
+            mkdir($compileDir, 0777, true);
+        }
+        if (!is_writable($compileDir)) {
+            chmod($compileDir, 0777);
+        }
+
         $this->smarty = new Smarty();
         $this->smarty->setTemplateDir(__DIR__ . '/../../templates');
-        $this->smarty->setCompileDir(__DIR__ . '/../../templates_c');
+        $this->smarty->setCompileDir($compileDir);
         $this->smarty->setCacheDir(__DIR__ . '/../../cache');
     }
 
